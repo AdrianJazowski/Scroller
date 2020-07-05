c31645fe1f07cc1764847db9a80fffd03e6bac82
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-07-05 19:03:05 +0200

    add scroll feature

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Hello Wordl");
-
   const rootElement = document.querySelector("#root");
-  const sections = document.querySelectorAll("secton");
+  const sections = document.querySelectorAll("section");
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
-  document.addEventListener("mousewheel", function (e) {});
+  document.addEventListener("mousewheel", function (event) {
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+    }, 1000);
+
+    const direction = event.wheelDelta < 0 ? 1 : -1;
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const firstSection = currentSectionIndex === 0;
+      if (firstSection) return;
+    }
+
+    currentSectionIndex = currentSectionIndex + direction;
+
+    console.log(currentSectionIndex);
+    console.log(currentSectionIndex);
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 });
