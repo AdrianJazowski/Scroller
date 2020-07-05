8b8841e2b11d708f65c4e074ba2fa5bd59b7b5d6
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-07-05 18:39:00 +0200

    Add normalize css, basic styles and javascript variables

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Hello Wordl");
+
+  const rootElement = document.querySelector("#root");
+  const sections = document.querySelectorAll("secton");
+
+  document.addEventListener("mousewheel", function (e) {});
+});
