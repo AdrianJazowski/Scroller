055ef96351ca9833cd45e621d4a1834228fb36bc
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-07-06 21:00:40 +0200

    Fix styles of img tag, add swiper to detect swipes on touchtable devices

--- Scroller.js
+++ Scroller.js
@@ -25,7 +25,7 @@ class Scroller {
     return isVissible;
   }
 
-  listenScroll = (event) => {
+  listenScroll(event) {
     if (this.isThrottled) return;
     this.isThrottled = true;
 
@@ -33,12 +33,12 @@ class Scroller {
       this.isThrottled = false;
     }, 1000);
 
-    const direction = event.wheelDelta < 0 ? 1 : -1;
+    const direction = event.deltaY > 0 ? 1 : -1;
 
     this.scroll(direction);
-  };
+  }
 
-  scroll = (direction) => {
+  scroll(direction) {
     if (direction === 1) {
       const isLastSection =
         this.currentSectionIndex === this.sections.length - 1;
@@ -50,17 +50,17 @@ class Scroller {
 
     this.currentSectionIndex = this.currentSectionIndex + direction;
     this.scrollToCurrentSection();
-  };
+  }
 
-  scrollToCurrentSection = () => {
+  scrollToCurrentSection() {
     this.selectActiveNavItem();
     this.sections[this.currentSectionIndex].scrollIntoView({
       behavior: "smooth",
       block: "start",
     });
-  };
+  }
 
-  drawNavigation = () => {
+  drawNavigation() {
     this.navigationContainer = document.createElement("aside");
     this.navigationContainer.setAttribute("class", "scroller__navigation");
     const list = document.createElement("ul");
@@ -80,9 +80,9 @@ class Scroller {
 
     document.body.appendChild(this.navigationContainer);
     this.selectActiveNavItem();
-  };
+  }
 
-  selectActiveNavItem = () => {
+  selectActiveNavItem() {
     if (this.navigationContainer) {
       const navigationItems = this.navigationContainer.querySelectorAll("li");
       navigationItems.forEach((item, index) => {
@@ -93,5 +93,5 @@ class Scroller {
         }
       });
     }
-  };
+  }
 }
